dd25f681541cb2e3e10c62a95ac505c1998065da
Zilde Souto Maior Neto <zsmn@cin.ufpe.br>
2023-05-15 10:05:51 -0300

    feat: spring base project

---
+++ src/test/java/com/br/zsmn/serverless/ServerlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zsmn.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
